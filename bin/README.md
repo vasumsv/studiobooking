585f46f9b4875499d818e7901a414889aca81123
vasumsv <quickproject1312@gmail.com>
2025-12-02 22:46:53 +0530

    Inital setup is done

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# studiobooking
+studiobooking
---
+++ src/test/java/com/studiobooking/studiobooking/StudiobookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiobooking.studiobooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiobookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
